5ad0711558c94fb79d8ef21dea13c0bcd5a30283
JaniBT <janoscsordas072@gmail.com>
2024-04-15 19:59:18 +0200

    dev

--- home_page/scripts/scripts.js
+++ home_page/scripts/scripts.js
@@ -2,3 +2,38 @@ function openDropdown(x) {
     x.classList.toggle('change')
     document.getElementById('dropdown-menu').classList.toggle('show')
 }
+
+const buttons = document.querySelectorAll('[data-carousel-button]')
+
+function carouselImageChanging(button) {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const carousel = button.closest('[data-carousel]')
+    const slides = carousel.querySelector('[data-slides]')
+    
+    const activeSlide = carousel.querySelector('[data-active]')
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    setInterval(() => {
+        newIndex += 1
+    }, 10000)
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+
+    const indicators = carousel.querySelectorAll('[data-indicator]')
+    indicators.forEach((indicator, index) => {
+        if (index === newIndex) {
+            indicator.dataset.active = true
+        } else {
+            delete indicator.dataset.active
+        }
+    })
+}
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        carouselImageChanging(button)
+    })
+})
