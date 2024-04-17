f25d556773a3c37e64eadfae0e06b62d57a8d3c9
Koós Balázs Bence <koosbalazsbence@gmail.com>
2024-04-17 20:07:19 +0200

    update-babel, create-another-book

--- books-page/script/script.js
+++ books-page/script/script.js
@@ -1,8 +1,35 @@
 
-// Navbar
+/* Navbar */
 
 function openDropdown(x) {
     x.classList.toggle('change')
     document.getElementById('dropdown-menu').classList.toggle('show')
 }
-// /Navbar
+/* **Navbar** */
+
+function toStock(){
+    alert("Jelenleg fejlesztés alatt! Kérjük szíves türelmét.")
+}
+
+/* display/hide content */
+let firstBtnClick = true
+
+function toggleText() {
+    let x = document.getElementsByClassName("full-description")
+    if (firstBtnClick) {
+        for (let i = 0; i < x.length; i++) {
+            x[i].classList.toggle("show")
+            x[i].style.display = "block"
+        }
+        firstBtnClick = false;
+    } else {
+        for (let i = 0; i < x.length; i++) {
+            if (x[i].style.display === "none") {
+                x[i].style.display = "block"
+            } else {
+                x[i].style.display = "none"
+            }
+        }
+    }
+}
+/* **display/hide content** */
